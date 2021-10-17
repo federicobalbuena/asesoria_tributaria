f48e793cc8a3c1e0263a605788d5f3f3af1797ff
federicobalbuena <92692343+federicobalbuena@users.noreply.github.com>
2021-10-17 17:30:34 -0300

    Add files via upload

---
+++ js/validaform.js
@@ -0,0 +1,41 @@
+window.onload = iniciar;
+
+function iniciar() {
+  document.getElementById('enviar').addEventListener('click', validate, false);
+}
+
+function validarNombre() {
+  var elemento = document.getElementById('nombre');
+  if (elemento.value ==''){
+    alert('El campo Nombre no puede estar vacío');
+    return false
+  }
+  return true;
+}
+
+function validarTel() {
+    var elemento = document.getElementById('tel');
+      if (isNaN(elemento.value)){
+        alert('Formato incorrecto, en el campo Teléfono debe ingresar números');
+        return false
+      }
+      return true;
+    }
+    
+    function validarCheck() {
+    var elemento = document.getElementById('aceptar');
+    if (!elemento.checked){
+      alert('Es necesario aceptar los términos y condiciones');
+      return false;
+    }
+    return true;
+    }
+    
+    function validate(e){
+      if(validarNombre() && validarTel() && validarCheck() && confirm('¿Seguro desea suscribirse?')){
+        return true;
+      } else {
+        e.preventDefault();
+        return false;
+      }
+    }
